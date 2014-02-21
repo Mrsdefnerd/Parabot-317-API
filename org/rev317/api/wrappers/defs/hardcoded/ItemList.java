ff81e2a50702b23d85560ec6b7a2f2d0d41944e5
Mrsdefnerd <owner@epic-pk.com>
2014-02-21 11:22:44 +0100

    Fixed up a lot + added a few new methods
    Fixed a prayer, added a few new methods

---
+++ org/rev317/api/wrappers/defs/hardcoded/ItemList.java
@@ -0,0 +1,10 @@
+package org.rev317.api.wrappers.defs.hardcoded;
+
+public class ItemList {
+	public int itemId;
+	public String itemName;
+
+	public ItemList(int _itemId) {
+		itemId = _itemId;
+	}
+}
